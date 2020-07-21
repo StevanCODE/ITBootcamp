b33ffcce4997e2d76d1f6a2f1459a14e6013e0f2
StevanCODE <pop.stevan@gmail.com>
2020-07-21 21:25:31 +0200

    mislio sam da je ovo za nedelju a ne za sredu i napravio izmene u cetvrtom zadatku, dodao while loop

---
+++ Nedelja2/Sreda/zadatak3.js
@@ -0,0 +1,13 @@
+x = 0
+if (x != 0){
+    if(x % 3 == 0){
+        console.log("Broj je deljiv sa 3")
+    }
+    else{
+        console.log("Broj nije deljiv sa 3")
+    }
+}
+else{
+    console.log("Ukucajte broj veci od 0")
+}
+
---
+++ Nedelja2/Sreda/zadatak4.js
@@ -0,0 +1,31 @@
+
+// function range(start, end) {
+//     return new Array(end - start).fill().map((d, i) => i + start);
+// }
+
+
+// Ovako sam u ponedeljak radio zbog Pythona jer tamo moze for i in range(1000): samo sto u JS nema ugradjena range funkcija
+
+// // for (i in range(1, 1005)) {         
+// //     if (i > 1 && i % 5 == 0) {
+// //         console.log(i)
+// //     }
+// // }
+
+
+
+n = 0
+
+// while (n <= 1000){
+//     n++
+//     if(n % 5=== 0){
+//         console.log(n)
+//     }
+// }
+
+
+for(n; n <=1000; n++){
+    if(n % 5 === 0){
+        console.log(n)
+    }
+}
