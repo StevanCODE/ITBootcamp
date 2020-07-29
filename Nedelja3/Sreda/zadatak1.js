cf6f375c51f4dcbebef864431c8a31203f37bad1
StevanCODE <pop.stevan@gmail.com>
2020-07-29 13:56:51 +0200

    Dodao jos jedan nacin resavanja

--- Nedelja3/Sreda/zadatak1.js
+++ Nedelja3/Sreda/zadatak1.js
@@ -1,21 +1,39 @@
-function maksimum(x,y,z){
-    let max
-    if(x > y && x > z){
-        max = x
-        return `X je najveci broj sa vrednoscu ${max}` 
-    }
-    else if(y > x && y > z){
-        max = y
-        return `Y je najveci broj sa vrednoscu ${max}`
-    }
-    else if(z > x && z > y){
-        max = z
-        return `Z je najveci broj sa vrednoscu ${max}`
-    }
-    else{
-        return "Brojevi su jednaki"
-    }
+// function maksimum(x,y,z){
+//     let max
+//     if(x > y && x > z){
+//         max = x
+//         return `X je najveci broj sa vrednoscu ${max}` 
+//     }
+//     else if(y > x && y > z){
+//         max = y
+//         return `Y je najveci broj sa vrednoscu ${max}`
+//     }
+//     else if(z > x && z > y){
+//         max = z
+//         return `Z je najveci broj sa vrednoscu ${max}`
+//     }
+//     else{
+//         return "Brojevi su jednaki"
+//     }
     
+// }
+
+function maksimum(x,y,z){
+    let max = []
+    max.push(x,y,z)
+    let rezultat = 0    
+    for(i = 0; i < max.length; i++){
+        if(x == y && x == z && z == y){
+            return "Brojevi su jednaki"
+    }
+        else if(max[i] > rezultat){
+            rezultat = max[i]
+        }
+            
+    
+    }
+return `Najveci broj je ${rezultat}`
 }
 
-console.log(maksimum(6,7,9))
+
+console.log(maksimum(56,89,56))
